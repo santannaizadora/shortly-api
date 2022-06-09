f4bd453085e30df22012ffd1e25ecf2b75d7d792
santannaizadora <santannaizadora@gmail.com>
2022-06-08 23:10:39 -0300

    feat: register route, no data validation

---
+++ src/controllers/registerController.js
@@ -0,0 +1,13 @@
+import connection from "../db.js";
+import CryptoJS from "crypto-js";
+
+export const signup = async (req, res) => {
+    const { name, email, password } = req.body;
+    const hashedPassword = CryptoJS.AES.encrypt(JSON.stringify(password), process.env.SECRET_KEY).toString();
+    const query = {
+        text: "INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *",
+        values: [name, email, hashedPassword]
+    };
+    const result = await connection.query(query);
+    res.status(201).json(result.rows[0]);
+}
