f586cec0fc748c03b029b7b86a8830f5c1b0db91
Mateus Brasil <Mateus-code-98>
2022-06-21 11:51:34 -0300

    Preparando revisão de código

--- src/routes/loginRouter.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { validateLogin } from "../middlewares/dataValidationMiddleware.js";
-import { checkIfUserExists, checkIfpasswordIsCorrect } from "../middlewares/loginMiddleware.js";
-import { signin} from "../controllers/loginController.js";
-
-const router = Router();
-router.post("/signin", validateLogin, checkIfUserExists, checkIfpasswordIsCorrect, signin);
-
-export default router;
--- src/routes/rankingRouter.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { ranking } from "../controllers/rankingController.js";
-
-const router = Router();
-
-router.get("/ranking", ranking)
-
-export default router;
